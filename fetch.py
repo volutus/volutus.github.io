e61e0bd5d2fbe30748fefdbba65803dd635c6275
volutus <trevcat3@hotmail.com>
2024-04-04 19:57:29 -0400

    Update fetch to use current date

--- fetch.py
+++ fetch.py
@@ -1,11 +1,12 @@
 import requests
+from datetime import datetime
 
+today = datetime.today().strftime('%Y-%m-%d')
 
 def standings(): 
-    uri = "https://api-web.nhle.com/v1/standings/2024-03-18"
+    uri = f"https://api-web.nhle.com/v1/standings/{today}"
     response = requests.get(uri)
     with open('json/standings.json', 'wb') as f:
         f.write(response.content)
-
 
 standings()
